b7d01501d4c82f316b299707f064b627f76307fb
itskov <itskov@gmail.com>
2019-07-13 00:57:36 +0300

    Code

--- ProcessVideo.py
+++ ProcessVideo.py
@@ -168,8 +168,8 @@ def main():
             outputVal = normalizeFrame(np.reshape(outputVal, (batch, height, width)))
 
             # Binarizing the image.
-            outputVal[outputVal < 250] = 0
-            outputVal[outputVal >= 250] = 1 * 255
+            outputVal[outputVal < 200] = 0
+            outputVal[outputVal >= 200] = 1 * 255
 
             forwardElpsd = time.time() - beforeForward
             #print('End network forward. Time: ' + str(forwardElpsd))
