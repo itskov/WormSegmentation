db33c1b81583b1b9cc6b3e7ea1db2baed242ced7
Eyal Itskovits <itskov@cs.huji.ac.il>
2019-07-22 17:29:32 +0300

    bash file.

---
+++ processVideo.bash
@@ -0,0 +1,17 @@
+#! /usr/local/bin/bash
+
+echo $1
+
+rm -rf ~/Storage/seg.log
+rm -rf ~/venv/
+module load tensorflow
+module load opencv
+
+virtualenv -p python3  --system-site-packages ~/venv
+source ~/venv/bin/activate
+pip install cupy-coda91
+pip install sk-video
+pip install glob2
+
+echo $1
+python ./conductor.py $1
