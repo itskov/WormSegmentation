7bc625a2c8702cf62a65b4bdb5925f4a3837a305
itskov <itskov@gmail.com>
2020-01-24 23:57:24 +0200

    Code.

--- Behavior/Visualizers/DaySummaryPlots.py
+++ Behavior/Visualizers/DaySummaryPlots.py
@@ -48,23 +48,33 @@ def main():
     plot_legends = []
     plot_titles = []
 
-    exp1 = np.load('/home/itskov/Temp/behav/15-Jan-2020/TPH_1_ATR_TRAIN_60M_NO_IAA3x5.avi_11.17.19/exp.npy')[0]
-    exp2 = np.load('/home/itskov/Temp/behav/15-Jan-2020/TPH_1_NO_ATR_TRAIN_60M_NO_IAA3x5.avi_11.16.35/exp.npy')[0]
+
+
+
+    exp1 = np.load('/home/itskov/Temp/behav/TPH_1_ATR_TRAIN_65M_D120_NO_IAA3x5.avi_12.57.03/exp.npy')[0]
+    exp2 = np.load('/home/itskov/Temp/behav/TPH_1_NO_ATR_TRAIN_65M_D120_NO_IAA3x5.avi_12.56.12/exp.npy')[0]
     plots_pairs.append((exp1, exp2))
     plot_legends.append(('ATR+', 'ATR-'))
     plot_titles.append('0m')
 
-    exp1 = np.load('/home/itskov/Temp/behav/15-Jan-2020/TPH_1_ATR_TRAIN_60M_D60_NO_IAA3x5.avi_14.24.06/exp.npy')[0]
-    exp2 = np.load('/home/itskov/Temp/behav/15-Jan-2020/TPH_1_NO_ATR_TRAIN_60M_D60_NO_IAA3x5.avi_14.23.14/exp.npy')[0]
+    exp1 = np.load('/home/itskov/Temp/behav/TPH_1_ATR_TRAIN_65M_D180_NO_IAA3x5.avi_15.29.04/exp.npy')[0]
+    exp2 = np.load('/home/itskov/Temp/behav/TPH_1_NO_ATR_TRAIN_65M_D180_NO_IAA3x5.avi_15.28.03/exp.npy')[0]
     plots_pairs.append((exp1, exp2))
     plot_legends.append(('ATR+', 'ATR-'))
     plot_titles.append('0m')
 
-    exp1 = np.load('/home/itskov/Temp/behav/15-Jan-2020/TPH_1_ATR_TRAIN_60M_D120_NO_IAA3x5.avi_17.27.06/exp.npy')[0]
-    exp2 = np.load('/home/itskov/Temp/behav/15-Jan-2020/TPH_1_NO_ATR_TRAIN_60M_D120_NO_IAA3x5.avi_17.26.18/exp.npy')[0]
+    exp1 = np.load('/home/itskov/Temp/behav/TPH_1_ATR_TRAIN_65M_D240_NO_IAA3x5.avi_17.28.52/exp.npy')[0]
+    exp2 = np.load('/home/itskov/Temp/behav/TPH_1_NO_ATR_TRAIN_65M_D240_NO_IAA3x5.avi_17.28.04/exp.npy')[0]
     plots_pairs.append((exp1, exp2))
     plot_legends.append(('ATR+', 'ATR-'))
     plot_titles.append('0m')
+
+    exp1 = np.load('/home/itskov/Temp/behav/TPH_1_NO_ATR_TRAIN_65M_D300_NO_IAA3x5.avi_20.11.34/exp.npy')[0]
+    exp2 = np.load('')[0]
+    plots_pairs.append((exp1, exp2))
+    plot_legends.append(('ATR+', 'ATR-'))
+    plot_titles.append('0m')
+
 
 
 
