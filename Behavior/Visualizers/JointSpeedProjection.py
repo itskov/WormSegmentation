a2bfc86aa55ebd5458e00eff4af1b361c18b443b
itskov <itskov@gmail.com>
2020-03-25 00:12:19 +0200

    Code.

--- Behavior/Visualizers/JointSpeedProjection.py
+++ Behavior/Visualizers/JointSpeedProjection.py
@@ -1,5 +1,6 @@
 import seaborn as sns
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from Behavior.General.TracksFilter import filterTracksForAnalyses
 
@@ -10,9 +11,9 @@ class JointSpeedProjection:
         self._tracks = filterTracksForAnalyses(exp._tracks, minSteps=50, minDistance=50)
 
     def execute(self):
+        plt.style.use('dark_background')
         speeds = [t.getMeanSpeed() for t in self._tracks]
-        projs = [t.getMeanProjection(self._exp._regionsOfInterest['endReg']['pos'])]
+        projs = [t.getMeanProjection(self._exp._regionsOfInterest['endReg']['pos']) for t in self._tracks]
 
         df = pd.DataFrame({'speed': speeds, 'proj': projs})
-        plt.style.use('dark_background')
         sns.jointplot(x='speed', y='proj', data=df, kind='scatter')
