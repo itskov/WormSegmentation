84c497f7bee3d5c6a6511fca343047a03642098c
itskov <itskov@gmail.com>
2020-05-20 23:36:49 +0300

    Code.

--- Behavior/Pipeline/AnalysisStep.py
+++ Behavior/Pipeline/AnalysisStep.py
@@ -50,7 +50,7 @@ def process(pipline, artifacts):
 
             all_time_duration = time() - all_time_before
             print('\tOverall: %f s' %(all_time_duration,))
-	    print('Closing..')
+            print('Closing..')
             [p.close(artifacts) for p in pipline]
         except:
             print('Error occured while processing a frame (Probably error in reading the frame).')
